5d9b1022dd514fb852d4bfc886d93444c7044fe0
brian lewis <brianburkelewis@gmail.com>
2018-06-14 09:24:55 -0700

    Score line endings' diversity

--- poetics/evolve_bigrams.py
+++ poetics/evolve_bigrams.py
@@ -18,16 +18,39 @@ import nltk
 import numpy as np
 
 
+def line_to_dict(line):
+    """
+    Determine import features of the given line.
+
+    Accepts tokenized lines, e.g. ['and', 'that', 'which', 'governs', 'me', 'to', 'go', 'about'].
+    """
+    return {
+        'raw_text': line,
+        'tokens': nltk.word_tokenize(line.lower()),
+        'ends_sentence': line[-1] in {'.', ';', '?', '!'},
+    }
+
+import kenlm
+path = 'data/lm_csr_5k_nvp_2gram/lm_csr_5k_nvp_2gram.arpa'
+path = 'data/lm_csr_64k_nvp_3gram/lm_csr_64k_nvp_3gram.arpa'
+klm = kenlm.LanguageModel(path)
+
 TEXT_FILEPATH = 'data/sonnets.txt'
 with open(TEXT_FILEPATH, 'r') as f:
     ALL_LINES = f.read().split('\n')
+ALL_LINES = [line for line in ALL_LINES if line]
+ALL_LINES = [line_to_dict(line) for line in ALL_LINES]
 
+for previous_line, line in zip(ALL_LINES, ALL_LINES[1:]):
+    line['starts_sentence'] = previous_line['ends_sentence']
+
+ALL_LINES[0]['starts_sentence'] = True
 
 CORPUSES = nltk.corpus.gutenberg.fileids()
 BIGRAM_FREQUENCY = collections.Counter()
 for fileid in CORPUSES:
     words = nltk.corpus.gutenberg.words(fileid)
-    bigrams = nltk.ngrams(words, n=2)
+    bigrams = nltk.ngrams([word.lower() for word in words], n=2)
     BIGRAM_FREQUENCY += collections.Counter(bigrams)
 
 
@@ -43,29 +66,121 @@ def replace_random_line(individual, source):
     return individual
 
 
+def mutate(individual, source):
+    """Replace a random number of lines with randomly chosen ones."""
+    for i in range(random.randint(0, int(len(individual) / 2.0))):
+        replace_random_line(individual, source)
+    return individual
+
+
 def evaluate(individual):
+    """Score an individual according to several evaluation methods."""
+    methods, weights = zip(*[
+        (evaluate_bigram_frequency, 1.0),
+        (evaluate_line_endings, 10.0),
+        (evaluate_poem_ending, 5.0),
+        (evaluate_poem_start, 5.0)
+    ])
+    score = sum(method(individual) * weight for method, weight in zip(methods, weights))
+    return (score,)
+
+
+def evaluate(individual):
+    """Score an individual according to several evaluation methods."""
+    poem_as_str = ' '.join([line['raw_text'] for line in individual])
+    sentences = nltk.sent_tokenize(poem_as_str)
+    score = sum(klm.score(sentence, eos=True, bos=True) for sentence in sentences)
+    if len(sentences) < 4:
+        score += -1000.0
+    return (score,)
+
+
+def evaluate(individual):
+    """Score an individual according to several evaluation methods."""
+    poem_as_str = ' '.join([line['raw_text'] for line in individual])
+    sentences = nltk.sent_tokenize(poem_as_str)
+    score = sum(klm.score(sentence, eos=True, bos=True) for sentence in sentences)
+    if len(sentences) < 4:
+        score += -1000.0
+    return (score,)
+
+
+
+def evaluate(individual):
+    """Score an individual according to several evaluation methods."""
+    poem_as_str = ' '.join([line['raw_text'] for line in individual])
+    sentences = nltk.sent_tokenize(poem_as_str)
+    score = (-1) * sum(klm.perplexity(sentence) for sentence in sentences)
+
+    if len(sentences) < 4:
+        score += -2000.0
+
+    # punish sentences > 3 lines long:
+    n_long_sentences = 0
+    count = 0
+    for line in individual:
+        try:
+            if not (line[-1] == '.'):
+                count += 1
+        except KeyError:
+            continue
+        else:
+            count = 0
+        if count > 3:
+            n_long_sentences += 1
+    score += n_long_sentences * (-3000.0)
+
+    raw_lines = [line['raw_text'] for line in individual]
+    n_duplicate_lines = len(raw_lines) - len(set(raw_lines))
+    score += n_duplicate_lines * (-2000.0)
+    return (score,)
+
+
+
+def evaluate_bigram_frequency(individual):
     """
     Use the observed bigram frequency to evaluate the reasonability of the line breaks.
 
     Used to score an individual's fitness.
     """
     # FIXME: This strongly favors poems whose lines begin with 'And'.
-    score = 5.0
+    score = 0.0
     for line, next_line in zip(individual, individual[1:]):
-        if line and line[-1] not in {'.', ';'}:
-            # FIXME: Tokenize outside of this step.
-            line_as_words = nltk.word_tokenize(line.lower())
-            next_line_as_words = nltk.word_tokenize(next_line.lower())
-            if line_as_words and next_line_as_words:
-                bigram_upper = (line_as_words[-1], next_line_as_words[0])
-                bigram_lower = (line_as_words[-1], next_line_as_words[0].lower())
-                score_upper = BIGRAM_FREQUENCY[bigram_upper]
-                score_lower = BIGRAM_FREQUENCY[bigram_lower]
-                score_to_add = max(score_upper, score_lower)
-                if score_to_add > 0:
-                    score += math.log(score_to_add)
+        if not line['ends_sentence']:
+            bigram = (line['tokens'][-1], next_line['tokens'][0].lower())
+            score += math.floor(math.sqrt(math.log(BIGRAM_FREQUENCY[bigram] + 1)))
+        elif line['ends_sentence'] and next_line['starts_sentence']:
+            score += 2.0
+    return score
 
-    return (score,)
+
+def evaluate_line_endings(individual):
+    """
+    Favor a healthy mix of distinct line endings by punishing imbalance.
+
+    Returns a score in the range [-len(individual), 0].
+    """
+    period_count = sum(line['ends_sentence'] for line in individual)
+    comma_count = sum(1 for line in individual if line['tokens'][-1] in {',', ':', '--'})
+    other_count = len(individual) - period_count - comma_count
+    return min(period_count, comma_count, other_count) \
+        - max(period_count, comma_count, other_count)
+
+
+def evaluate_poem_ending(individual):
+    """Favor individuals that end on a full-stop."""
+    try:
+        return float(individual[-1]['tokens'][-1] == '.')
+    except (KeyError, IndexError):
+        return 0.0
+
+
+def evaluate_poem_start(individual):
+    """Favor individuals that start with a sentence-starting line."""
+    try:
+        return individual[0]['starts_sentence']
+    except (KeyError, IndexError):
+        return 0.0
 
 
 def initialize_toolbox():
@@ -80,7 +195,7 @@ def initialize_toolbox():
 
     toolbox.register('evaluate', evaluate)
     toolbox.register('mate', tools.cxOnePoint)
-    toolbox.register('mutate', lambda x: (replace_random_line(x, ALL_LINES),))
+    toolbox.register('mutate', lambda x: (mutate(x, ALL_LINES),))
     toolbox.register('select', tools.selTournament, tournsize=3)
 
     return toolbox
@@ -88,8 +203,8 @@ def initialize_toolbox():
 
 def evolve_grammatical_poems(toolbox):
     """Let evolution take its course for the given toolbox."""
-    population = toolbox.population(n=5000)
-    hof = tools.HallOfFame(10)
+    population = toolbox.population(n=8000)
+    hof = tools.HallOfFame(100)
     stats = tools.Statistics(lambda ind: ind.fitness.values)
     stats.register('avg', np.mean)
     stats.register('min', np.min)
@@ -98,9 +213,9 @@ def evolve_grammatical_poems(toolbox):
     population, logbook = algorithms.eaSimple(
         population=population,
         toolbox=toolbox,
-        cxpb=0.5,
+        cxpb=0.50,
         mutpb=0.2,
-        ngen=30,
+        ngen=50,
         stats=stats,
         halloffame=hof,
         verbose=True
@@ -111,4 +226,6 @@ def evolve_grammatical_poems(toolbox):
 if __name__ == '__main__':
     toolbox = initialize_toolbox()
     population, logbook, hof = evolve_grammatical_poems(toolbox)
-    print(hof[0])
+
+    for line in hof[0]:
+        print(' '.join(line['tokens']))
